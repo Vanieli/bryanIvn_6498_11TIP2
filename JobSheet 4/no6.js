4d067d6f21fb04153bbfd96e76527e4b1a699d4f
Vanieli <113155548+Vanieli@users.noreply.github.com>
2023-08-09 09:49:04 +0700

    -

---
+++ JobSheet 4/no6.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Masukkan sebuah kalimat: ", (inputString) => {
+    rl.question("Masukkan indeks yang ingin Anda cek: ", (index) => {
+        index = Number(index); // Konversi nilai index ke tipe Number
+
+        if (index >= 0 && index < inputString.length) {
+            const character = inputString.charAt(index); // Mendapatkan karakter pada indeks yang diminta
+            console.log(`Karakter pada indeks ${index}: ${character}`);
+        } else {
+            console.log("Indeks tidak valid!");
+        }
+
+        rl.close();
+    });
+});
---
+++ Latihan/properti_string_lenght.js
@@ -0,0 +1,15 @@
+// Lenght
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Masukkan sebuah kalimat: ", inputString => {
+    //
+    console.log(`Panjang kalimat: ${inputString.length}`);
+
+    rl.close();
+});
+
+
